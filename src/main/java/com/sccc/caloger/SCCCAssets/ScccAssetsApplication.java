92be2a92eab1c4b172d05fcb9eb268ba38c66e68
Matthew Caloger <mattcaloger@gmail.com>
2018-05-18 00:19:33 -0400

    Initial commit
    Generated setup.

---
+++ src/main/java/com/sccc/caloger/SCCCAssets/ScccAssetsApplication.java
@@ -0,0 +1,12 @@
+package com.sccc.caloger.SCCCAssets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScccAssetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScccAssetsApplication.class, args);
+	}
+}
